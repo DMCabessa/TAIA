277df3e8ecc4c3d2669de2cdbe612b54b5204a73
Natalia Cometti <npvc@cin.ufpe.br>
2015-04-15 22:52:09 -0300

    Fix in getAny

--- AG/getAny.m
+++ AG/getAny.m
@@ -2,10 +2,10 @@ function parent = getAny(options,parentA)
 
 idx = randi(options.PopulationSize) ;
 
-if options.Population(idx,:) ~= parentA
-	parent = options.Population(idx,:) ;
+if options.Population(idx) ~= parentA
+	parent = options.Population(idx) ;
 elseif idx < options.PopulationSize
-	parent = options.Population(idx+1,:) ;
+	parent = options.Population(idx+1) ;
 else
-	parent = options.Population(1,:) ;
+	parent = options.Population(1) ;
 end
